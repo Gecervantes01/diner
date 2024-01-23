a7a638f127a0d88ef605bf2d63f2ca241c548ea7
Giovan Cervantes <cervantes.giovan@student.greenriver.edu>
2024-01-23 13:36:17 -0500

    Added more routes

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /* Giovan Cervantes
  * January 2024
  * https://gecervantes.greenriverdev.com/328/diner/
@@ -17,7 +18,47 @@ $f3 = Base::instance();
 
 // Define a default route
 $f3 ->route('GET /', function() {
-    echo "This is my Diner";
+
+    // Display a view page
+    $view = new Template();
+    echo $view -> render('views/home.html');
+});
+
+$f3 ->route('GET /breakfast', function() {
+
+    //echo "Breakfast";
+    // Display a view page
+    $view = new Template();
+    echo $view -> render('views/breakfast-menu.html');
+});
+
+$f3 ->route('GET|POST /form-one', function($f3) {
+    // If form has been posted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // validate the data
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+
+        // put the data in the session array
+        $f3->set('SESSION.food', $food);
+        $f3->set('SESSION.meal', $meal);
+
+        // redirect to order2 route
+        $f3->reroute('summary');
+    }
+
+    // Display a view page
+    $view = new Template();
+    echo $view -> render('views/order-form-1.html');
+});
+
+$f3 ->route('GET /summary', function() {
+
+    echo "Thank you for your order!";
+    // Display a view page
+//    $view = new Template();
+//    echo $view -> render('views/.html');
 });
 
 // Run Fat-Free
